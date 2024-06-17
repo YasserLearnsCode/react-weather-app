dc99fa2de50055daa66f1dff4995638e8167831a
YasserLearnsCode <147191695+YasserLearnsCode@users.noreply.github.com>
2024-06-17 10:56:19 -0700

    added footer links

--- src/App.js
+++ src/App.js
@@ -3,8 +3,24 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-    <h1>React Weather App</h1>
+      <h1>React Weather App</h1>
 
+      <footer>
+        <p>
+          This App was coded by{" "}
+          <a
+            href="https://yasserkabulimbo.netlify.app"
+            alt="Front-end developer Yasser Kabulimbo's web porfolio"
+          >
+            Yasser Kabulimbo. {" "}
+          </a>
+        It is open sourced on{" "}
+          <a href="https://github.com/YasserLearnsCode/react-weather-app" alt="github repository for the weather app">
+            Github
+          </a>
+          , & hosted on Netlify.
+        </p>
+      </footer>
     </div>
   );
 }
